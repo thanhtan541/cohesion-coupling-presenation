47fcbaded441db40f9c6bf09a2788bb9b134f886
thanhtan541 <thanhtan004100@gmail.com>
2025-09-09 14:09:46 +0700

    feat: update content

--- src/cohesion.rs
+++ src/cohesion.rs
@@ -1,7 +1,22 @@
+//! Cohesion is a measure of the degree to which the elements of the module are functionally related.
+//! It is the degree to which all elements directed towards performing a single task are contained in the component.
+//! Basically, cohesion is the internal glue that keeps the module together.
+//! A good software design will have high cohesion.
+
+#[rustfmt::skip]
+mod function_cohesion;
 mod coincidental_cohesion;
 mod communicational_cohesion;
-mod function_cohesion;
 mod logical_cohesion;
 mod procedural_cohesion;
 mod sequence_cohesion;
 mod temporal_cohesion;
+
+// mod function_cohesion;
+// mod sequence_cohesion;
+// mod communicational_cohesion;
+// mod procedural_cohesion;
+// mod temporal_cohesion;
+// mod logical_cohesion;
+// mod coincidental_cohesion;
+//
--- src/coupling.rs
+++ src/coupling.rs
@@ -1,12 +1,20 @@
 //! ./coupling.rs
 //!
-//! This module introduces 6 types of coupling and organize the code from low to high.
-//! The low coupling will be laid at the top and go down
+//! Coupling is the measure of the degree of interdependence between the modules.
+//! A good software will have low coupling.
+//!
 
 #[rustfmt::skip]
 mod data_coupling; // High
-mod common_coupling; // ..
+mod common_coupling;
 mod content_coupling;
-mod control_coupling; // ..
-mod external_coupling; //..
-mod stamp_coupling; // .. // Low
+mod control_coupling;
+mod external_coupling;
+mod stamp_coupling; // Low
+//
+// mod data_coupling; // High
+// mod stamp_coupling;
+// mod control_coupling;
+// mod external_coupling;
+// mod common_coupling;
+// mod content_coupling; // Low
